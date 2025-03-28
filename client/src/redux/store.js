a083509c800475552272183c13bceec128424b5c
singh27aditi <s27aditi@gmail.com>
2025-03-28 19:21:32 +0530

    Added Google OAuth Functionality

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,13 +4,15 @@ import { persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import persistStore from 'redux-persist/es/persistStore'
 
-const rootReducer = combineReducers({user: userReducer})
+const rootReducer = combineReducers({user: userReducer});
+
 const persistConfig = {
     key: 'root',
     storage,
-    version: 1,
+    version: 1
 }
-const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
   reducer: persistedReducer,
